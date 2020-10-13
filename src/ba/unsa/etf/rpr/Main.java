18f87aa073818e42f08443b93f1baf3af1148ecd
Kerim Bavcic <kbavcic1@etf.unsa.ba>
2020-10-13 14:43:09 +0200

    Promijenjen tekst koji se ispisuje na ekran

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Podravit cemo sve koji gledaju ovo");
+        System.out.println("Podrav za sve");
     }
 }
